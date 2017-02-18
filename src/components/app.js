b2f8b57f46a07fdfcdbf8f438b959cf71b884b73
Sam Goldsmith <samggold@gmail.com>
2017-02-18 11:58:53 -0700

    fetching trending gif data

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Header from './Header';
 
-const GIPHYURL='https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC';
+const GIPHY_URL='https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC';
 
 export default class App extends Component {
   constructor(props) {
@@ -13,13 +14,31 @@ export default class App extends Component {
       gifs: []
     }
 
-    this.getData = this.getData.bind(this);
+  }
+
+  getData(gifs) {
+    axios.get(GIPHY_URL)
+      .then(res => {
+        const gifs = res.data.data;
+        this.setState({ gifs });
+      })
+  }
+
+  componentDidMount(gifs) {
+    this.getData(gifs);
   }
 
   render() {
     return (
       <div>
         <Header />
+        <div>
+          {this.state.gifs.map(el => {
+            return (
+              <h6 key={el.id}><img src={el.bitly_gif_url}></img></h6>
+            )
+          })}
+        </div>
       </div>
     );
   }
