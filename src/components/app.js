434b0c2d9e51d8d874930743bc67200dbcfdd9f0
Sam Goldsmith <samggold@gmail.com>
2017-02-18 11:07:31 -0700

    App constructor to fetch gifs

--- src/components/app.js
+++ src/components/app.js
@@ -2,9 +2,20 @@ import React, { Component } from 'react';
 
 import Header from './Header';
 
-const GIPHYURL='https://api.giphy.com/v1/gifs/trending';
+const GIPHYURL='https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC';
 
 export default class App extends Component {
+  constructor(props) {
+
+    super();
+
+    this.state = {
+      gifs: []
+    }
+
+    this.getData = this.getData.bind(this);
+  }
+
   render() {
     return (
       <div>
