1cfe04041ed8f652cff4bfa22ac545d2777e9734
Sam Goldsmith <samggold@gmail.com>
2017-02-21 16:34:03 -0700

    comments for some logic

--- src/components/App.js
+++ src/components/App.js
@@ -10,13 +10,14 @@ export default class App extends Component {
   constructor(props) {
     super(props);
 
+    //initial state has empty array for ajax response payload
+    //initial state has startAt set to 0 representing first index of gif array. Intention is to have event handle onclick which incrementally loads gif elements
     this.state = {
       trendingGifs: [],
-      startAt: 1
+      startAt: 0
     }
 
     this.renderCards = this.renderCards.bind(this);
-    //this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount(data) {
@@ -30,25 +31,31 @@ export default class App extends Component {
       });
   }
 
-  // handleClick() {
-  //   //display 5 more gif cards
-  // }
-
   createCard(props) {
     let key = props.id;
     let source = props.images.fixed_height.url;
-    return <Card src={source} key={key} />
+    let alt = props.url;
+    return <Card src={source} key={key} alt={alt}/>
   }
 
+  //render Card component dynamically, however, only render first 10 elements, allow user to load more on click -tbd
   renderCards(arr) {
     let start = this.state.startAt;
     return arr.slice(start, 10).map(this.createCard);
   }
 
   render() {
+    //indicate gifs are loading while state is set in component mount, gifs array is empty
     if (!this.state.trendingGifs.length) {
-      return <div className="loading">Loading...</div>
+      return (
+        <div className="load-indicator">
+          <svg className="load" x="0px" y="0px" viewBox="0 0 150 150">
+            <circle id="loading-circle" cx="75" cy="75" r="60"/>
+          </svg>
+        </div>
+      )
     } else {
+      //gifs loaded, render cards
       return (
         <main>
            <ul className="card-container">
--- src/components/Card.js
+++ src/components/Card.js
@@ -5,11 +5,12 @@ import ShareButtons from './Social';
 const Card = (props) => {
   let img_url = props.src;
   let key = props.index;
+  let altText = props.alt
 
   return (
     <li key={key} className="card-wrapper">
       <section className="card">
-        <img src={img_url}/>
+        <img src={img_url} alt={altText}/>
         <ShareButtons />
       </section>
     </li>
