5977dc8b5a5633a9fabf9a776814c9019a829574
Sam Goldsmith <samggold@gmail.com>
2017-02-21 15:38:23 -0700

    removed handleClick from App

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,7 @@ export default class App extends Component {
     }
 
     this.renderCards = this.renderCards.bind(this);
-    this.handleClick = this.handleClick.bind(this);
+    //this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount(data) {
@@ -30,30 +30,24 @@ export default class App extends Component {
       });
   }
 
-  handleClick() {
-    console.log('help');
-    //e.preventDefault();
-    // let newStart = this.state.StartAt + 5;
-    // this.setState({startAt: newStart }, () => {
-    //   this.onClick({renderCards});
-    // })
-  }
+  // handleClick() {
+  //   //display 5 more gif cards
+  // }
 
   createCard(props) {
     let key = props.id;
     let source = props.images.fixed_height.url;
-    let tw_share = `http://twitter.com/share?text=Get%20This%20TopGif&url=${props.bitly_gif_url}`;
-    return <Card src={source} key={key} href={`${tw_share}`}/>
+    return <Card src={source} key={key} />
   }
 
   renderCards(arr) {
     let start = this.state.startAt;
-    return arr.slice(start, 5).map(this.createCard);
+    return arr.slice(start, 10).map(this.createCard);
   }
 
   render() {
     if (!this.state.trendingGifs.length) {
-      return <div>Uh oh, something went wrong!</div>
+      return <div className="loading">Loading...</div>
     } else {
       return (
         <main>
