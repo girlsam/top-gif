35b0d2c7ee3e79a032c74972bf49fea75ae5c964
Sam Goldsmith <samggold@gmail.com>
2017-09-14 21:02:01 -0600

    Reorganized file structure with buttons folder

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 import Card from './Card';
-import LoadMoreButton from './LoadMoreButton';
-import MyFaveGifs from './MyGifs';
+import LoadMoreButton from './buttons/LoadMoreButton';
+import MyFaveGifs from './buttons/MyGifsButton';
 import PageEnd from './PageEnd';
 
 import { getCookie, setCookie } from './../helpers';
@@ -16,13 +16,15 @@ export default class App extends Component {
 
     this.state = {
       trendingGifs: [],
-      startAt: 0
+      startAt: 0,
+      showMyGifs: false
     }
 
-    this.renderCards = this.renderCards.bind(this);
-    this.handleLoadMoreClick = this.handleLoadMoreClick.bind(this);
-    this.createCard = this.createCard.bind(this);
     this.addCookieClick = this.addCookieClick.bind(this);
+    this.createCard = this.createCard.bind(this);
+    this.getMyGifs = this.getMyGifs.bind(this);
+    this.handleLoadMoreClick = this.handleLoadMoreClick.bind(this);
+    this.renderCards = this.renderCards.bind(this);
   }
 
   componentDidMount(data) {
@@ -68,12 +70,18 @@ export default class App extends Component {
     setCookie('faves', cookie);
   }
 
+  getMyGifs() {
+    let cookie = getCookie('faves');
+    let img = cookie.split('=')[1];
+  }
+
   //create function to toggle <i> class onClick
 
   render() {
     //indicate gifs are loading while state is set in component mount, gifs array is empty
     return (
       <div>
+        <MyFaveGifs getGifs={this.getMyGifs}/>
         {
           !this.state.trendingGifs.length ? (
             <div className="spinner"></div>
--- src/components/Default404.js
+++ src/components/Default404.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Default404 = () => {
-  return <div>Not Found... 404</div>;
+  return <div>Sorry, we couldn't find the GIFs you're looking for. Come back soon, though, for more GIFs.</div>;
 }
 
 export default Default404;
