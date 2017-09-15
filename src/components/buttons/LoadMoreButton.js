86dc03b04d5b7ce588ce17fad3e6a69ca0e26db3
Sam Goldsmith <samggold@gmail.com>
2017-09-14 21:01:01 -0600

    reorganized file structure with buttons folder

---
+++ src/components/buttons/LoadMoreButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadMoreButton = (props) => {
+  return (
+    <div className="btn">
+      <button className="btn-load-more" onClick={props.onClick}>GIF Me More</button>
+    </div>
+  )
+}
+
+export default LoadMoreButton;
