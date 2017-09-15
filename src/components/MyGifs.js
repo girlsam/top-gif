0664c1ed967c8355aed3ea955f8a30502357b777
Sam Goldsmith <samggold@gmail.com>
2017-09-14 20:32:40 -0600

    New component container for favorite gifs

---
+++ src/components/MyGifs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MyFaveGifs = (props) => {
+  return (
+    <div className="btn">
+      <button className="btn-load-more">
+        My Fave Gifs
+      </button>
+    </div>
+  )
+}
+
+export default MyFaveGifs;
